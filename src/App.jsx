fc8470487e2d805fd62dde3831a4b211e3ddebe7
Al Khalid <awfulgaming997@gmail.com>
2023-08-29 20:00:37 +0530

    updated dark mode toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-
-
-import { BrowserRouter , Routes , Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
 import Project from "./pages/Project";
 import Navbar from "./Navbar";
-import './App.css'
+
+import "./App.css";
 
 function App() {
- 
-
   return (
-<BrowserRouter>
-<Navbar />
-<Routes>
-  <Route path="/" element={<Home />} />
-  <Route path="/Portfolio" element={<Portfolio />} />
-  <Route path="/Contact" element={<Contact />} />
-  <Route path="/Project" element={<Project />} />
-</Routes>
-</BrowserRouter>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Portfolio" element={<Portfolio />} />
+        <Route path="/Contact" element={<Contact />} />
+        <Route path="/Project" element={<Project />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
